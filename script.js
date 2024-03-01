210a372be9b398d018e6504b464b468b7842d811
Hao Le <71927521+HaoQLe@users.noreply.github.com>
2024-03-01 00:11:17 -0800

    Implement computer choice for RPS

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+function getComputerChoice() {
+    computerChoice = getRandomNumber(0, 2);
+
+    if (computerChoice === 0) {
+        return "Rock";
+    } else if (computerChoice === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+
+}
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
