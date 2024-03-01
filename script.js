fbc3af2d2083b5999c9425141f15727345268666
Hao Le <71927521+HaoQLe@users.noreply.github.com>
2024-03-01 00:21:17 -0800

    Implement fucntion to play single round of RPS.

--- script.js
+++ script.js
@@ -14,3 +14,54 @@ function getComputerChoice() {
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock") {
+        switch (computerSelection) {
+            case "Rock":
+                return "You tied."
+                break;
+            case "Paper":
+                return "You lose! Paper beats your rock.";
+                break;
+            case "Scissors":
+                return "You win! Your rock beats scissors.";
+                break;
+        }
+    }
+
+    if (playerSelection === "paper") {
+        switch (computerSelection) {
+            case "Rock":
+                return "You win! Your paper beats rock."
+                break;
+            case "Paper":
+                return "You tied.";
+                break;
+            case "Scissors":
+                return "You lose! Scissors beats your paper.";
+                break;
+        }
+    }
+
+    if (playerSelection === "scissors") {
+        switch (computerSelection) {
+            case "Rock":
+                return "You lose! Rock beats your scissors."
+                break;
+            case "Paper":
+                return "You win! Your scissors beats paper.";
+                break;
+            case "Scissors":
+                return "You tied.";
+                break;
+        }
+    }
+
+
+}
+
+const playerSelection = "scissors";
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
